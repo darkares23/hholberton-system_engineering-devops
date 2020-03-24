b250187ed84f4cbfc3ddb5a90ab3038b449ec8da
darkares23 <desarrollojuseca@gmail.com>
2020-03-24 16:37:38 -0500

    fixed task 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # set up your client SSH configuration file so that you can connect to a server without typing a password.
 file_line { 'Remove passwd authentication':
   ensure => 'present',
-  path => '/etc/ssh/ssh_config',
+  path => '~/ssh/ssh_config',
   line => 'PasswordAthentication no',
   }
 file_line { 'Identity File':
   ensure => 'present',
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile /juan/.ssh/holberton',
+  path => '~/ssh/ssh_config',
+  line => '/juan/.ssh/holberton',
 }
