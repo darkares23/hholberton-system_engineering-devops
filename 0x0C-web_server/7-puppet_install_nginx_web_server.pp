013059a45d4951d4cc062e030ca6022179152fa4
darkares23 <desarrollojuseca@gmail.com>
2020-03-25 17:17:12 -0500

    la cague ;(

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# puppet file that installs and configures nginx
+package {'nginx':
+  ensure => installed,
+}
+
+file {'/var/www/html/index.html':
+    content => 'Holberton School',
+}
+
+file_line { 'title':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _',
+  line   => 'rewrite ^/redirect_me https://www.facebook.com permanent;',
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
