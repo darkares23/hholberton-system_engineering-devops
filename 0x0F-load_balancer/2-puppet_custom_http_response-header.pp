d4c517a7e9599aa1f74a5ea9e6c162c80263b3fe
darkares23 <desarrollojuseca@gmail.com>
2020-04-03 18:28:15 -0500

    task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,18 @@
+# puppet file that creates a custom HTTP header response
+package {'nginx':
+  ensure  => installed,
+  name    => 'nginx',
+}
+
+file_line { 'header':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'add_header X-Served-By "$HOSTNAME";',
+  multiple => true
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
