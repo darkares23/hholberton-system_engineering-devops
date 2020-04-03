dd0893544348eafe897fed99736dc5cc9f5bf356
darkares23 <desarrollojuseca@gmail.com>
2020-04-03 18:36:58 -0500

    added task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,18 +1,5 @@
-# puppet file that creates a custom HTTP header response
-package {'nginx':
-  ensure  => installed,
-  name    => 'nginx',
-}
-
-file_line { 'header':
-  ensure   => present,
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'server_name _;',
-  line     => 'add_header X-Served-By "$HOSTNAME";',
-  multiple => true
-}
-
-service {'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+#automate the task of creating a custom HTTP header response
+exec { 'HTTP Configuration':
+  command  => "apt-get update && apt-get -y install nginx && echo -e 'Holberton School' > sudo /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://facebook.com permanent;' /etc/nginx/sites-available/default && sed -i '22i add_header X-Served-By $hostname;' /etc/nginx/nginx.conf && service nginx start",
+  provider => 'shell',
 }
