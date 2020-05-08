9d68c6142ed3c6c35c2a19062b41cf0573fe8940
darkares23 <desarrollojuseca@gmail.com>
2020-05-07 19:37:36 -0500

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns
-a list containing the titles of all hot articles for a given subreddit.
-If no results are found for the given subreddit, the function should
-return None.
-"""
+
 
 import json
 from requests import get
